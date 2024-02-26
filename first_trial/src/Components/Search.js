574f4f10468520f958e5f68894012f5830b13e98
darrk-heart <prospaul999@gmail.com>
2024-02-26 20:01:09 +0000

    search component done

---
+++ first_trial/src/Components/Search.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        //height: '100vh'
+      }}
+    >
+      <input
+        style={{ width: "100px" }}
+        type="text"
+        placeholder="Search ...."
+      ></input>
+      <div
+        style={{ marginTop: "10px", display: "flex", alignItems: "flex-start" }}
+      >
+        <label style={{ fontWeight: "600" }}>
+          <input type="checkbox" />
+          Only show products in stock
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
