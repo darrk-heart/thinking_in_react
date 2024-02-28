becea1bdb1258fbfde750d4f06741bad8949b2e0
darrk-heart <prospaul999@gmail.com>
2024-02-28 08:38:53 +0000

    done with the top

--- age_calculator/src/Components/DateInput.js
+++ age_calculator/src/Components/DateInput.js
@@ -1,23 +1,38 @@
 import React from "react";
 import "./DateInput.css";
+import arrow from "../Assets/icon-arrow.svg";
 
 function DateInput() {
   return (
-    <div className="dateinput">
-      <div className="head">
-        <span className="heading">DAY</span>
-        <input placeholder="DD"></input>
+    <div>
+      <div className="first">
+        <div className="head">
+          <span className="heading">DAY</span>
+          <input placeholder="DD"></input>
+        </div>
+        <div className="head">
+          <span className="heading">MONTH</span>
+          <input placeholder="MM"></input>
+        </div>
+        <div className="head">
+          <span className="heading">YEAR</span>
+          <input placeholder="YYYY"></input>
+        </div>
       </div>
-      <div className="head">
-        <span className="heading">MONTH</span>
-        <input placeholder="MM"></input>
-      </div>
-      <div className="head">
-        <span className="heading">YEAR</span>
-        <input placeholder="YYYY"></input>
+      <div className="first one">
+        <div className="line"></div>
+        <div>
+          <img
+            className="arrow"
+            src={arrow}
+            alt="You click this to see your date of Birth"
+          />
+        </div>
       </div>
     </div>
   );
 }
 
 export default DateInput;
+
+// src\Assets\icon-arrow.svg
