c5c4c88e0488e22ce5cb2d5038b4dee36729db22
darrk-heart <prospaul999@gmail.com>
2024-02-28 05:02:56 +0000

    started with components

---
+++ age calculator/second/src/Components/Background.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Background() {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "70vh",
+        width: "50%",
+        borderRadius: "20px 20px 150px 20px",
+      }}
+    ></div>
+  );
+}
+
+export default Background;
