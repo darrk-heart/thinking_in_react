200f633b9a75c1e2bb1365d25d19b513f88234b5
darrk-heart <prospaul999@gmail.com>
2024-02-28 11:34:53 +0000

    front end code checked

--- age_calculator/src/Components/DateInput.js
+++ age_calculator/src/Components/DateInput.js
@@ -1,95 +1,73 @@
 import React, { useState } from "react";
 import "./DateInput.css";
-import arrow from "../Assets/icon-arrow.svg";
+import arrowIcon from "../Assets/icon-arrow.svg";
 
-const isValidDay = (value) => {
-  return !isNaN(value) && value >= 1 && value <= 31;
-};
-const isValidMonth = (value) => {
-  return !isNaN(value) && value >= 1 && value <= 12;
-};
-const isValidYear = (value) => {
-  return !isNaN(value) && value >= 1920 && value <= 2023;
-};
+const isValidInput = (value, min, max) =>
+  !isNaN(value) && value >= min && value <= max;
+const isValidDay = (value) => isValidInput(value, 1, 31);
+const isValidMonth = (value) => isValidInput(value, 1, 12);
+const isValidYear = (value) => isValidInput(value, 1920, 2023);
 
 function DateInput() {
   const [dayValue, setDayValue] = useState("");
   const [monthValue, setMonthValue] = useState("");
   const [yearValue, setYearValue] = useState("");
 
-  const handleDayChange = (e) => {
+  const handleInputChange = (setValue, isValid, min, max) => (e) => {
     const value = e.target.value;
-    if (isValidDay(value)) {
-      setDayValue(value);
-    }
-  };
-
-  const handleMonthChange = (e) => {
-    const value = e.target.value;
-    if (isValidMonth(value)) {
-      setMonthValue(value);
-    }
-  };
-
-  const handleYearChange = (e) => {
-    const value = e.target.value;
-    if (isValidYear(value)) {
-      setYearValue(value);
+    if (isValid(value)) {
+      setValue(value);
     }
   };
 
   return (
     <div>
       <div className="first">
-        <div className="head">
-          <span className="heading">DAY</span>
-          <input
-            type="number"
-            id="dayInput"
-            value={dayValue}
-            onChange={handleDayChange}
-            min="1"
-            max="31"
-            placeholder="DD"
-          ></input>
-        </div>
-        <div className="head">
-          <span className="heading">MONTH</span>
-          <input
-            type="number"
-            id="monthInput"
-            value={monthValue}
-            onChange={handleMonthChange}
-            min="1"
-            max="12"
-            placeholder="MM"
-          ></input>
-        </div>
-        <div className="head">
-          <span className="heading">YEAR</span>
-          <input
-            type="number"
-            id="yearInput"
-            value={yearValue}
-            onChange={handleYearChange}
-            min="1920"
-            max="2023"
-            placeholder="YYYY"
-          ></input>
-        </div>
+        <InputField
+          label="DAY"
+          value={dayValue}
+          onChange={handleInputChange(setDayValue, isValidDay, 1, 31)}
+          placeholder="DD"
+        />
+        <InputField
+          label="MONTH"
+          value={monthValue}
+          onChange={handleInputChange(setMonthValue, isValidMonth, 1, 12)}
+          placeholder="MM"
+        />
+        <InputField
+          label="YEAR"
+          value={yearValue}
+          onChange={handleInputChange(setYearValue, isValidYear, 1920, 2023)}
+          placeholder="YYYY"
+        />
       </div>
-      <div className="first one">
-        <div className="line"></div>
-        <div>
-          <img
-            className="arrow"
-            src={arrow}
-            alt="You click this to see your date of Birth"
-          />
-        </div>
-      </div>
+      <SeparatorWithArrow />
     </div>
   );
 }
+
+const InputField = ({ label, value, onChange, placeholder }) => (
+  <div className="head">
+    <span className="heading">{label}</span>
+    <input
+      type="number"
+      value={value}
+      onChange={onChange}
+      min={1}
+      max={label === "YEAR" ? 2023 : 12}
+      placeholder={placeholder}
+    />
+  </div>
+);
+
+const SeparatorWithArrow = () => (
+  <div className="first one">
+    <div className="line"></div>
+    <div>
+      <img className="arrow" src={arrowIcon} alt="Click to see date of birth" />
+    </div>
+  </div>
+);
 
 export default DateInput;
