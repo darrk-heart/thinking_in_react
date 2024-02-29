b4d764ce9e3c093e78706202e658a823b26dbf84
darrk-heart <prospaul999@gmail.com>
2024-02-29 07:30:15 +0000

    main functionalites working

--- age_calculator/src/Components/DateInput.js
+++ age_calculator/src/Components/DateInput.js
@@ -9,12 +9,38 @@ function DateInput() {
   const [dayValue, setDayValue] = useState("");
   const [monthValue, setMonthValue] = useState("");
   const [yearValue, setYearValue] = useState("");
+  const [age, setAge] = useState(null);
 
   const handleInputChange = (e, setter, min, max) => {
     const value = e.target.value;
     if (isValidInput(value, min, max)) {
       setter(value);
     }
+  };
+
+  const calculateAge = () => {
+    const currentDate = new Date();
+    const birthDate = new Date(`${yearValue}-${monthValue}-${dayValue}`);
+    let years = currentDate.getFullYear() - birthDate.getFullYear();
+    let months = currentDate.getMonth() - birthDate.getMonth();
+    let days = currentDate.getDate() - birthDate.getDate();
+
+    if (months < 0 || (months === 0 && days < 0)) {
+      years--;
+      months += 12;
+    }
+
+    if (days < 0) {
+      const tempDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        0
+      );
+      days = tempDate.getDate() - birthDate.getDate() + currentDate.getDate();
+      months--;
+    }
+
+    setAge({ years, months, days });
   };
 
   return (
@@ -64,9 +90,17 @@ function DateInput() {
             className="arrow"
             src={arrow}
             alt="You click this to see your date of Birth"
+            onClick={calculateAge}
           />
         </div>
       </div>
+      {/* Display age */}
+      {age !== null && (
+        <p>
+          Your age is {age.years} years, {age.months} months, and {age.days}{" "}
+          days old.
+        </p>
+      )}
     </div>
   );
 }
